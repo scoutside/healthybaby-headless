7af78c57172ff4be9eb11bcd1352829efd59f7f0
Tyler Patton <jamestylerpatton@gmail.com>
2022-07-06 15:45:32 -0700

    build: initial commit from starter
    Create from blank next init
    Pull components and utils from Nacelle starter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // add image domains here as needed, for next/image
+    domains: ['cdn.shopify.com'],
+  },
 }
 
 module.exports = nextConfig
